d66e8d5c07d1448a64bda1c503c453c81389e359
shinebird <949232047@qq.com>
2026-05-08 00:27:54 +0800

    [Server] [HTTP] 为Request添加defaultHandler

--- lib/controller/http/UserServiceController.cpp
+++ lib/controller/http/UserServiceController.cpp
@@ -8,15 +8,42 @@ namespace netdisk::core::http::controller
 {
     namespace request
     {
+        namespace internal
+        {
+            template <typename Body, bool DiscardBody>
+            static auto defaultHandler(
+                boost::beast::http::request_parser<boost::beast::http::empty_body>& parser,
+                boost::asio::ssl::stream<boost::beast::tcp_stream>& stream,
+                boost::beast::flat_buffer& buffer) -> boost::asio::awaitable<Request>
+            {
+                if constexpr (!DiscardBody)
+                {
+                    boost::beast::http::request_parser<Body> new_parser{std::move(parser)};
+                    co_await boost::beast::http::async_read(stream, buffer, new_parser,
+                                                            boost::asio::use_awaitable);
+                    co_return pro::make_proxy<proxy::Request>(std::move(new_parser.get()));
+                }
+                else
+                {
+                    boost::beast::http::request_parser<boost::beast::http::dynamic_body> new_parser{
+                        std::move(parser)};
+                    while (!new_parser.is_done())
+                    {
+                        co_await boost::beast::http::async_read_some(stream, buffer, new_parser,
+                                                                     boost::asio::use_awaitable);
+                    }
+                    co_return pro::make_proxy<proxy::Request>(std::move(new_parser.get()));
+                }
+            }
+        } // namespace internal
+
         auto test(boost::beast::http::request_parser<boost::beast::http::empty_body>& parser,
                   boost::asio::ssl::stream<boost::beast::tcp_stream>& stream,
                   boost::beast::flat_buffer& buffer, const boost::urls::matches& match)
             -> boost::asio::awaitable<Request>
         {
-            boost::beast::http::request_parser<boost::beast::http::dynamic_body> new_parser{
-                std::move(parser)};
-            co_await boost::beast::http::async_read(stream, buffer, new_parser, boost::asio::use_awaitable);
-            co_return pro::make_proxy<proxy::Request>(std::move(new_parser.get()));
+            co_return co_await internal::defaultHandler<boost::beast::http::dynamic_body, true>(
+                parser, stream, buffer);
         }
     } // namespace request
     namespace response
